6f3b2c66a044a1cae317ea24ca3d620fc19f7a41
Benjamin Beret <kouak@macbook-de-benjamin-beret.local>
2009-11-30 16:58:51 +0100

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+puts "Création des secteurs ..."
+["2F", "3E", "4R", "FIR", "3H", "4N"].each do |secteur|
+  puts "#{secteur} ..."
+  Secteur.find_or_create_by_name(secteur)
+end
+puts "Ok !"
+
+puts "Création des équipes ..."
+(1..12).each do |equipe|
+  puts "#{equipe} ..."
+  Equipe.find_or_create_by_equipe(equipe)
+end
+puts "Ok !"
